2fac487bfea6cd42f335ea5e617267319848ee40
saria-mohi <saria.m.mohammed@gmail.com>
2023-09-28 23:17:43 +0300

    task3:course try to fix error

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,43 +1,39 @@
-class HolbertonCourse {
+export default class HolbertonCourse {
+
   constructor(name, length, students) {
     this._name = name;
     this._length = length;
     this._students = students;
   }
 
-  get name() {
+  get getName() {
     return this._name;
   }
-
-  set name(newName) {
-    if (typeof newName === 'string') {
-      this._name = newName;
-    } else {
+  set setName(name) {
+    if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
     }
+    this._name = name;
   }
 
-  get length() {
+  get getLength() {
     return this._length;
   }
 
-  set length(newLength) {
-    if (typeof newLength === 'number') {
-      this._length = newLength;
-    } else {
+  set setLength(length) {
+    if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
     }
+    this._length = length;
   }
-
-  get students() {
+  get getStudents() {
     return this._students;
   }
 
-  set students(newStudents) {
-    if (Array.isArray(newStudents)) {
-      this._students = newStudents;
-    } else {
+  set setStudents(std) {
+    if (!Array.isArray(val)) {
       throw new TypeError('Students must be an array');
     }
+    this._students = std;
   }
 }
